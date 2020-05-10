7ee28b1183829986b8eacece28c39f2aabf674c4
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-05-10 12:32:45 +0300

    start creating login & admin page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostsPageComponent } from './posts-page/posts-page.component';
+import { PostComponent } from './posts-page/post-page/post.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostsPageComponent,
+    PostComponent,
+    LoginPageComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
