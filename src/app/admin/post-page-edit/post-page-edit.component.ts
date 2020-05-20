a75b856241f5742c218b3ba5dbb3a2398cb66158
Nikita Tysiachnyi <nikita.tysiachnyi@gmail.com>
2020-05-21 01:56:35 +0300

    created admin/dashboard page, edit page connected 2, fixed route in admin part to lowercase

---
+++ src/app/admin/post-page-edit/post-page-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-page-edit',
+  templateUrl: './post-page-edit.component.html',
+  styleUrls: ['./post-page-edit.component.scss']
+})
+export class PostPageEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
