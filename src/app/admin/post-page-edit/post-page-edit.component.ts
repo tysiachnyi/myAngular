bd7f55fb1eda0cfee80338a1efc4a55a910e4185
Nikita <nikita.tysiachnyi@gmail.com>
2020-05-31 13:01:37 +0300

    fixed htmleditor in editPage and router to dashboard page in admin module

--- src/app/admin/post-page-edit/post-page-edit.component.ts
+++ src/app/admin/post-page-edit/post-page-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-post-page-edit',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page-edit.component.scss']
 })
 export class PostPageEditComponent implements OnInit {
+  public Editor = ClassicEditor;
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required)
+    });
+  }
+
+  submit() {
+    console.log('hi');
   }
 
 }
